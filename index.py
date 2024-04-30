d4cb8a804b2038dca4aa66be781399644e250661
andhika agung pratama <andhikaagung79@gmail.com>
2024-04-30 19:08:16 +0700

    Adding Query Parameter to the GET

--- index.py
+++ index.py
@@ -9,11 +9,20 @@ class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        num = self.get_argument("num")
+        if (num.isdigit()):
+            r = "odd" if int(num) % 2 else "even"
+            self.write(f"The integer {num} is {r}")
+        else:
+            self.write(f"{num} is not a valid integer.")
 
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
-        (r"/animal", listRequestHandler)
+        (r"/animal", listRequestHandler),
+        (r"/isEven", queryParamRequestHandler)
     ])
 
     port = 8882
