fccec2cb2642725355aa87322d089fc19b639690
andhika agung pratama <andhikaagung79@gmail.com>
2024-05-04 03:45:59 +0700

    Adding Dynamic Method

---
+++ dynamic post get/index.py
@@ -0,0 +1,28 @@
+import tornado.web
+import tornado.ioloop
+import json
+
+
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        fh = open("list.txt", "r")
+        fruits = fh.read().splitlines()
+        fh.close()
+        self.write(json.dumps(fruits))
+    
+    def post(self):
+       fruit = self.get_argument("fruit")
+       fh = open("list.txt", "a")
+       fh.write(f"{fruit}\n")
+       fh.close()
+       self.write(json.dumps({"message": "Fruit added successfully."}))
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/list", listRequestHandler),
+    ])
+
+    port = 8882
+    app.listen(8882)
+    print(f"Application is listening on port {port}")
+    tornado.ioloop.IOLoop.current().start()
